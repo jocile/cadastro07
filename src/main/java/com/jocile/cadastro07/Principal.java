9942cee04588506e990ac3ad0deada03a98af85f
jocile <jocile@gmail.com>
2023-06-19 21:23:42 -0300

    Renomeei a classe principal

---
+++ src/main/java/com/jocile/cadastro07/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jocile.cadastro07;
+
+/**
+ *
+ * @author aluno
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
